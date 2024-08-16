e197701b393c84ccf6379f63ef3aab1bfe1faf4f
Pedro Tozzi <pedrogbtozzi@gmail.com>
2024-08-16 14:38:50 -0300

    add PokeApiLive and PokeApiTest (i dont like it, its getting worse)

--- src/poke-api.ts
+++ src/poke-api.ts
@@ -1,8 +1,8 @@
-import type { ParseResult } from '@effect/schema'
-import { Context, Effect } from 'effect'
+import { ParseResult, Schema } from '@effect/schema'
+import { Config, Context, Effect } from 'effect'
 import type { ConfigError } from 'effect/ConfigError'
-import type { FetchError, JsonError } from './errors'
-import type { Pokemon } from './schemas'
+import { FetchError, JsonError } from './errors'
+import { Pokemon } from './schemas'
 
 export interface PokeApi {
     readonly getPokemon: Effect.Effect<
@@ -12,3 +12,35 @@ export interface PokeApi {
 }
 
 export const PokeApi = Context.GenericTag<PokeApi>('PokeApi')
+
+export const PokeApiLive = PokeApi.of({
+    getPokemon: Effect.gen(function* () {
+        const baseUrl = yield* Config.string('BASE_URL')
+
+        const response = yield* Effect.tryPromise({
+            try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
+            catch: () => new FetchError(),
+        })
+
+        if (!response.ok) {
+            return yield* new FetchError()
+        }
+
+        const json = yield* Effect.tryPromise({
+            try: () => response.json(),
+            catch: () => new JsonError(),
+        })
+
+        return yield* Schema.decodeUnknown(Pokemon)(json)
+    }),
+})
+
+export const PokeApiTest = PokeApi.of({
+    getPokemon: Effect.succeed({
+        id: 1,
+        height: 10,
+        weight: 10,
+        order: 1,
+        name: 'myname',
+    }),
+})
