5525ea1cdb7e11cc8876a2196648452259db341e
Pedro Tozzi <pedrogbtozzi@gmail.com>
2024-08-16 15:34:58 -0300

    feat: Add mock handler for fetching Pokemon data

---
+++ test/handlers.ts
@@ -0,0 +1,16 @@
+import { http, HttpResponse } from 'msw'
+import type { Pokemon } from '../src/schemas'
+
+const mockPokemon: Pokemon = {
+    id: 1,
+    height: 10,
+    weight: 10,
+    order: 1,
+    name: 'myname',
+}
+
+export const handlers = [
+    http.get('http://localhost:3000/api/v2/pokemon/garchomp/', () => {
+        return HttpResponse.json(mockPokemon)
+    }),
+]
