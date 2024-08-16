5747c2ad0c39716ea6cac036e08d9064d42d6f53
Pedro Tozzi <pedrogbtozzi@gmail.com>
2024-08-16 00:24:30 -0300

    tryPromise and flatMap

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { Console, Effect } from "effect";
+import { Console, Effect } from 'effect'
 import express from 'express'
 import { env } from '../src/env'
-
-const main = Console.info("Hello world");
-
-Effect.runSync(main);
 
 const app = express()
 
@@ -12,6 +8,18 @@ app.use(express.json())
 
 const PORT = env.NODE_ENV === 'production' ? 3000 : 4200
 
-const httpServer = app.listen(PORT, async () => {
+app.listen(PORT, async () => {
     console.log(`✔✔✔✔✔✔ Working at PORT: ${PORT} ✔✔✔✔✔✔`)
 })
+
+/// Effect<Response, UnknownException>
+const fetchRequest = Effect.tryPromise(() =>
+    fetch('https://pokeapi.co/api/v2/pokemon/garchomp/'),
+)
+
+/// Effect<unknown, UnknownException>
+const jsonResponse = (response: Response) =>
+    Effect.tryPromise(() => response.json())
+
+/// Effect<unknown, UnknownException>
+const main = Effect.flatMap(fetchRequest, jsonResponse)
